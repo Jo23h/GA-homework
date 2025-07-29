39cab39578d18ada64474f40a47e2ddbe1efd8f8
Jo23h <147503767+Jo23h@users.noreply.github.com>
2025-07-29 22:11:10 +0800

    new updates

--- dom-events-lab/app.js
+++ dom-events-lab/app.js
@@ -1,9 +1,50 @@
-/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button')
 
-/*-------------------------------- Variables --------------------------------*/
+let firstNumber = ''
+let operator = ''
+let secondNumber = ''
 
-/*------------------------ Cached Element References ------------------------*/
 
-/*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach (button => {
+    button.addEventListener('click', (event) => {
+        const buttonClicked = event.target;
+        const buttonValue = buttonClicked.textContent
 
-/*-------------------------------- Functions --------------------------------*/
+        if (buttonValue === 'C') {
+            firstNumber = ''
+            operator = ''
+            secondNumber = ''
+            console.log('cleared')
+        } 
+        
+        if (buttonValue === '='){
+             console.log('Equals pressed')
+            if (firstNumber && operator && secondNumber){
+                let num1 = Number(firstNumber)
+                let num2 = Number(secondNumber)
+                let result
+
+                if (operator === '+'){
+                    result = num1 + num2
+                } else if (operator === '-'){
+                    result = num1 - num2
+                } else if (operator === '/'){
+                    result = num1/num2
+                } else if (operator === '*'){
+                    result = num1*num2
+                }
+                console.log('Result:', result)
+            }
+        } 
+
+        if(buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/'){
+            operator = buttonValue
+            console.log('pressed',operator)
+        }
+         
+    })
+})
+
+
+
+
