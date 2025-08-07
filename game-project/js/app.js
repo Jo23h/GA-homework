0ba8a5082f45bc5ef30ca3c52d060d9673187d66
Jo23h <147503767+Jo23h@users.noreply.github.com>
2025-08-07 18:39:10 +0800

    update 7 Aug

--- game-project/js/app.js
+++ game-project/js/app.js
@@ -1,17 +1,101 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-console.log(c)
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 800
+canvas.height = 600
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+class Player{ 
+    constructor(){
+        this.position = {
+            x:0,
+            y:0,
+            velocity: 0,
+        }
+        this.velocity = {
+            x: 0,
+            y: 1,
+        }
+        this.height = 100;
+        this.width = 100;
+        }
+    
 
-let y = 100
-function animate(){
+   createPlayer(){
+        c.fillStyle = "red"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        }
+    
+    update(){
+        this.createPlayer()
+        this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
+
+        if (this.position.y + this.height < canvas.height){
+            this.velocity.y += 0.5
+        } else {
+            this.velocity.y = 0
+        }
+    }
+}
+
+const player = new Player()
+
+const keys = {
+    d: {
+        pressed: false, 
+    },
+    a: {
+        pressed: false,
+    }, 
+    w: {
+        pressed: false,
+    }
+}
+
+function animate() {
     window.requestAnimationFrame(animate)
-    c.fillStyle = 'red'
-    c.fillRect (200, y, 100, 100)
-    y++
+    c.fillStyle = 'white'
+    c.fillRect(0,0,canvas.width, canvas.height)
+    player.update()
+
+    player.velocity.x = 0
+    if(keys.d.pressed === true){
+        player.velocity.x = 1
+    } if (keys.a.pressed === true){
+        player.velocity.x = -1
+    } if (keys.w.pressed === true){
+        player.velocity.y = -10
+    }
+
 }
 animate()
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'd'){ 
+        keys.d.pressed = true
+    }
+
+    if (event.key === 'a'){ 
+        keys.a.pressed = true
+    }
+
+     if (event.key === 'w'){ 
+        keys.w.pressed = true
+    }
+
+})
+
+document.addEventListener('keyup', event => {
+    if (event.key === 'd'){ 
+        keys.d.pressed = false
+    }
+
+    if (event.key === 'a'){ 
+        keys.a.pressed = false
+    }
+
+     if (event.key === 'w'){ 
+        keys.w.pressed = false
+    }
+
+})
