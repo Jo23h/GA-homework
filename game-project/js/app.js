0e5e87b1d19d0a924b3467f73a127f0603ef3f68
Jo23h <147503767+Jo23h@users.noreply.github.com>
2025-08-10 17:52:42 +0800

    base game up

--- game-project/js/app.js
+++ game-project/js/app.js
@@ -1,101 +1,149 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 800
-canvas.height = 600
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
-class Player{ 
+// Create Player template
+class Player{
     constructor(){
-        this.position = {
-            x:0,
-            y:0,
-            velocity: 0,
-        }
-        this.velocity = {
-            x: 0,
-            y: 1,
-        }
-        this.height = 100;
-        this.width = 100;
-        }
-    
 
-   createPlayer(){
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        // Static position
+        this.position = {
+            x:100,
+            y:100,
         }
-    
-    update(){
-        this.createPlayer()
+        // Acceleration
+        this.velocity = {
+            x:0,
+            y:1,
+        }
+        // Player's hitbox
+        this.width = 30
+        this.height = 30
+    }
+
+    // Drawing Player hitbox and position
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    // Updating Player's position
+    refresh(){
         this.position.y += this.velocity.y
         this.position.x += this.velocity.x
+        this.draw()
 
-        if (this.position.y + this.height < canvas.height){
-            this.velocity.y += 0.5
-        } else {
-            this.velocity.y = 0
+        // Adding gravity effect
+        if (this.position.y + this.height + this.velocity.y <= canvas.height){
+            this.velocity.y += gravity
+        } 
+        // Stops Player from falling off the screen
+        else (this.velocity.y = 0)
+    }
+}
+
+// Create Plaform
+class Platform{
+    constructor({x,y}){
+        this.position = {
+            x: x,
+            y: y,
         }
+
+        this.width = 300
+        this.height = 20
+    }
+
+    draw(){
+        c.fillStyle = 'green'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
 
-const player = new Player()
-
+// Controls
 const keys = {
-    d: {
-        pressed: false, 
+    right: {
+        pressed: false
     },
-    a: {
-        pressed: false,
-    }, 
-    w: {
-        pressed: false,
+    left: {
+        pressed: false
     }
 }
-
-function animate() {
-    window.requestAnimationFrame(animate)
-    c.fillStyle = 'white'
-    c.fillRect(0,0,canvas.width, canvas.height)
-    player.update()
-
-    player.velocity.x = 0
-    if(keys.d.pressed === true){
-        player.velocity.x = 1
-    } if (keys.a.pressed === true){
-        player.velocity.x = -1
-    } if (keys.w.pressed === true){
-        player.velocity.y = -10
-    }
-
-}
-animate()
-
-document.addEventListener('keydown', event => {
-    if (event.key === 'd'){ 
-        keys.d.pressed = true
-    }
-
-    if (event.key === 'a'){ 
-        keys.a.pressed = true
-    }
-
-     if (event.key === 'w'){ 
-        keys.w.pressed = true
-    }
-
-})
 
 document.addEventListener('keyup', event => {
     if (event.key === 'd'){ 
-        keys.d.pressed = false
+        keys.right.pressed = false
     }
 
     if (event.key === 'a'){ 
-        keys.a.pressed = false
+        keys.left.pressed = false
     }
-
-     if (event.key === 'w'){ 
-        keys.w.pressed = false
-    }
-
 })
+
+
+document.addEventListener('keydown', event => {
+     if (event.key === 'w'){ 
+        player.velocity.y -= 20
+    }
+
+    if (event.key === 'a'){ 
+        keys.left.pressed = true
+    }
+
+    if (event.key === 'd'){ 
+        keys.right.pressed = true
+    }
+})
+
+
+// Create Player (1)
+const player = new Player()
+
+// Create Platforms
+const platforms = [new Platform({x: 200, y:100}), new Platform({x: 500, y: 200})]
+
+// Establish gravity pull
+const gravity = 0.5
+
+// Properties that change over time
+function animate(){
+
+    // Create gravity: Refresh window by clearing the canvas -> update Player's position 
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+    player.refresh()
+
+    // Enable Player left and right movement and stop Player from when key is not pressed
+    if (keys.right.pressed === true && player.position.x < 1000){
+        player.velocity.x = 5
+    } else if (keys.left.pressed === true && player.position.x > 100){
+        player.velocity.x = -5
+    } else (player.velocity.x = 0)
+
+    // Scroll map
+    if (keys.right.pressed === true){
+        platforms.forEach(platform => {
+        platform.position.x -= 5
+    })
+    } if (keys.left.pressed === true){
+        platforms.forEach(platform => {
+        platform.position.x += 5
+        })
+    }
+
+    // Collision detection
+    platforms.forEach((platform) => {
+        if (player.position.y + player.height <= platform.position.y && 
+        player.position.y + player.height + player.velocity.y >= platform.position.y &&
+        player.position.x + player.width >= platform.position.x &&
+        player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0
+    }
+    })  
+}
+animate()
